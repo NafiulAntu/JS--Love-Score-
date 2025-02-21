a6ad70a4870f92c897e6056e6d75b6d6f73d8692
nafiul-islam <157912936+NafiulAntu@users.noreply.github.com>
2025-02-21 17:11:03 +0600

    Update java-1.js

--- java-1.js
+++ java-1.js
@@ -6,10 +6,19 @@ loveScore = Math.floor(loveScore) + 1;
 
 if (loveScore > 70) {
   alert(
-    "Your love socre is" +
+    "Your love socre is " +
       loveScore +
       "%" +
       "  You love each other like Ayman love Farhan."
+  );
+}
+if (loveScore > 30 && loveScore <= 70) {
+  alert("Your love score is " + loveScore + "%");
+}
+
+if (loveScore <= 30) {
+  alert(
+    "Your love score is " + loveScore + "%" + "You together like oil and water."
   );
 } else {
   alert("Your love score is " + loveScore + "%");
